2cb513bc16fa4cda5048b8cd5408ceaf534cbb53
stefan-bergstein <stefan.bergstein@gmail.com>
2020-04-13 22:38:17 +0200

    Add call to anomaly_detection

--- components/iot-consumer/index.js
+++ components/iot-consumer/index.js
@@ -13,7 +13,9 @@ var topic_temperature = process.env.TOPIC_TEMPERATURE || "iot-sensor/sw/temperat
 var topic_vibration = process.env.TOPIC_VIBRATION || "iot-sensor/sw/vibration";
 
 // MQTT connection
-var mqtt_broker = process.env.MQTT_BROKER || "ws://broker-amq-mqtt-all-0-svc";
+// var mqtt_broker = process.env.MQTT_BROKER || "ws://broker-amq-mqtt-all-0-svc";
+var mqtt_broker = process.env.MQTT_BROKER || "ws://broker-amq-mqtt-all-0-svc-rte-iotdemo.apps-crc.testing";
+
 var mqtt_user = process.env.MQTT_USER || "iotuser";
 var mqtt_password = process.env.MQTT_PASSWORD || "iotuser";
 
@@ -22,6 +24,15 @@ const temperature_threshold = process.env.TEMPERATURE_THRESHOLD || 70.0;
 
 const temperature_alert_enabled = ((process.env.TEMPERATURE_ALERT_ENABLED || "true") === 'true');
 const vibration_alert_enabled = ((process.env.VIBRATION_ALERT_ENABLED || "true") === 'true');
+const vibration_anomaly_enabled = ((process.env.VIBRATION_ANOMALY_ENABLED || "true") === 'true');
+const vibration_anomaly_pump = process.env.VIBRATION_ANOMALY_PUMP || "floor-1-line-1-extruder-1pump-2";
+
+// const anomaly_detection_url = process.env.ANOMALY_DETECTION_URL || 'http://anomaly-detection-anomaly-detection';
+
+const anomaly_detection_url = process.env.ANOMALY_DETECTION_URL || 'https://anomaly-detection-iotdemo.apps-crc.testing';
+
+
+
 
 // setup application
 var mqtt = require('mqtt')
@@ -32,6 +43,7 @@ const http = require('http').createServer(app);
 // const router = new express.Router();
 const cors = require('cors');
 const io = require('socket.io')(http, { origins: '*:*', path: socket_path });
+const request = require('request-promise');
 
 // Middleware config
 app.use(cors());
@@ -93,23 +105,62 @@ function handleLight(message) {
 
 // Mock for Anomaly
 var last_value_map = {};
-function check_anomaly(id, value) {
+async function check_anomaly(id, value) {
     var result = false
 
-    if ( isNaN(last_value_map[id])) {
-       result = false
-       console.log('Last ID: %s,  Val: NO', id );
-    } else {
-       console.log('Last ID: %s,  Val: %d', id, last_value_map[id] );
-       if (  value > 2 && value > (last_value_map[id] * 1.95) ) {
-	      result = true
-       } else {
-         result = false
-       }
-    }
+    if (vibration_anomaly_enabled && id == vibration_anomaly_pump ) {
+        // Call anomaly detection web service
+        console.log('Anomaly detection for: %s, Val: %d', id, value );
+        var edgeAnomalyDict = { "data": { "ndarray": [parseFloat(value)] }};
 
-    console.log('New  ID: %s,  Val: %d', id, value );
-    last_value_map[id] = value;
+        log.debug("edgeAnomalyDict: " + JSON.stringify(edgeAnomalyDict)); //DELETE
+    
+        try {
+          if ( id != 'floor-1-line-1-extruder-1pump-2' ) {
+            result = false;
+            console.log('Last ID: %s,  Val: NO', id );
+          } else {
+            console.log('Last ID: %s,  Val: %d', id, value );
+            const edgeAnomalyResponse = await request({
+              method: 'POST',
+              uri: anomaly_detection_url + '/api/v0.1/predictions',
+              body: edgeAnomalyDict,
+              json: true,
+              timeout: 1000
+            });
+    
+            log.debug("Edge Anomaly Repsonse: " + JSON.stringify(edgeAnomalyResponse)); //DELETE
+            log.debug("Edge Anomaly Repsonse.data: " + JSON.stringify(edgeAnomalyResponse.data)); //DELETE
+    
+            if ( parseInt(edgeAnomalyResponse["data"]["ndarray"][0]) == 1 ){
+              result = true;
+            } else {
+              result = false;
+            }
+          }
+        } catch (err) {
+          log.error("check_anomaly failed", err)
+        }
+
+
+    } else {
+        // Basic internal anomaly check
+
+        if ( isNaN(last_value_map[id])) {
+        result = false
+        console.log('Last ID: %s,  Val: NO', id );
+        } else {
+        console.log('Last ID: %s,  Val: %d', id, last_value_map[id] );
+        if (  value > 2 && value > (last_value_map[id] * 1.95) ) {
+            result = true
+        } else {
+            result = false
+        }
+        }
+
+        console.log('New  ID: %s,  Val: %d', id, value );
+        last_value_map[id] = value;
+    }
     return result;
 }
 
@@ -138,7 +189,7 @@ function handleTemperature(message) {
     }
 }
 
-function handleVibration(message) {
+async function handleVibration(message) {
     console.log('handleVibration data %s', message);
     io.sockets.emit("vibration-event", message);
 
@@ -150,8 +201,8 @@ function handleVibration(message) {
         var id=elements[0]+elements[1]
         var value = parseFloat(elements[2])
 
-        var ano = check_anomaly(id,value)
-        console.log('Ano: %s', ano.toString());
+        var ano = await check_anomaly(id,value)
+        console.log('Ano: %s', ano);
 
         if(ano) {
             console.log('vibration alert!!!');
